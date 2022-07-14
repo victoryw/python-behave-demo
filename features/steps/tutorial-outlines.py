2336f397dd2270c9461beb5c0fdc28e91e923968
victoryw <victoryw@thoughtworks.com>
2022-07-14 22:50:55 +0800

    turorial step data

--- features/steps/tutorial-outlines.py
+++ features/steps/tutorial-outlines.py
@@ -16,3 +16,38 @@ def step_impl(context):
 @then("it should transform into (.+)")
 def step_impl(context, arg0):
     print('then is {0}'.format(arg0))
+
+
+class Model:
+    def __init__(self):
+        self.user = {}
+
+    def add_user(self, name, department):
+        self.user[name] = department
+
+    def find_user(self, name):
+        return list(filter(lambda x: (x == name), self.user.values()))
+
+
+@given("a set of specific users")
+def step_impl(context):
+    context.model = Model()
+    for row in context.table:
+        context.model.add_user(name=row['name'], department=row['department'])
+
+
+@when("we count the number of people in each department")
+def step_impl(context):
+    pass
+
+
+@then('we will find two people in "Silly Walks"')
+def step_impl(context):
+    out_list = context.model.find_user("Silly Walks")
+    assert len(out_list) == 2
+
+
+@step('we will find one person in "Beer Cans"')
+def step_impl(context):
+    out_list = context.model.find_user("Beer Cans")
+    assert len(out_list) == 1
--- features/tutorial-outlines.feature
+++ features/tutorial-outlines.feature
@@ -13,3 +13,14 @@ Feature: show case the outlines
    | thing         | other thing |
    | iPhone        | toxic waste |
    | Galaxy Nexus  | toxic waste |
+
+  Scenario: some scenario
+    Given a set of specific users
+       | name      | department  |
+       | Barry     | Beer Cans   |
+       | Pudey     | Silly Walks |
+       | Two-Lumps | Silly Walks |
+
+      When we count the number of people in each department
+      Then we will find two people in "Silly Walks"
+      But we will find one person in "Beer Cans"
