afb9b6328207255dbfe81ed289f4bdebef5f94b5
victoryw <victoryw@thoughtworks.com>
2022-07-15 12:56:04 +0800

    study step params for regex mode

--- features/step.feature
+++ features/step.feature
@@ -1,8 +1,8 @@
 Feature: show case the step implementation
   Scenario: look up a book
-  Given I search for a valid book
+  Given I search for a "valid" book
    Then the result page will include "success"
 
   Scenario: look up an invalid book
-    Given I search for a invalid book
+    Given I search for a "invalid" book
      Then the result page will include "failure"
--- features/steps/step.py
+++ features/steps/step.py
@@ -1,18 +1,17 @@
 from behave import *
 
-use_step_matcher("parse")
+use_step_matcher("re")
 
 
-@given("I search for a valid book")
-def step_impl(context):
-    context.response = "success"
+@given('I search for a "(?P<type>.*)" book')
+def step_impl(context, type):
+    switcher = {
+        "valid": "success",
+        "invalid": "failure"
+    }
+    context.response = switcher.get(type)
 
 
-@then('the result page will include "{text}"')
+@then('the result page will include "(?P<text>.*)"')
 def step_impl(context, text):
     assert context.response == text;
-
-
-@given("I search for a invalid book")
-def step_impl(context):
-    context.response = "failure"
